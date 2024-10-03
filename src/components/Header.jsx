155e63f540f48c3435e880dad6bcae1b9d2d07a5
Fitsumhelina <permin1501424@gmail.com>
2024-10-03 20:44:29 +0300

    header file created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <>
+    <header className="header">
+      <nav className="nav">
+
+        <div className="one">
+        <img src="" alt="" />
+        </div>
+
+        <div className="two">
+        <a href="#">About Us</a>
+        <a href="#">Programs</a>
+        <a href="#">Gallery</a>
+        <a href="#">Contact US</a>
+        </div>
+      </nav>
+    </header>
+    </>
   )
 }
 
