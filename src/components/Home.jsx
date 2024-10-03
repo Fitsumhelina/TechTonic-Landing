bde258e5f6e044b6ae85661760b5fb021bcda6f6
Fitsumhelina <permin1501424@gmail.com>
2024-10-03 21:05:05 +0300

    home page created

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import '../scss/Home.scss';
 
+const images = [
+  'https://media.licdn.com/dms/image/v2/D4E22AQFI83JUa-BSMw/feedshare-shrink_800/feedshare-shrink_800/0/1717747961232?e=1730937600&v=beta&t=UVFzZt49MUg6TjKscjYGhPzBnbVLjZDN5LPlAlZrDcc',
+  'https://media.licdn.com/dms/image/v2/D4E22AQEzczar5E4Phg/feedshare-shrink_800/feedshare-shrink_800/0/1717747961335?e=1730937600&v=beta&t=OM41JajwcPrlxS1MzbyDs6Fn4-U4eYolgCdGTfq0VRk',
+  'https://media.licdn.com/dms/image/v2/D4E22AQFAgijLxS9Hvw/feedshare-shrink_800/feedshare-shrink_800/0/1717747960918?e=1730937600&v=beta&t=2nkwVS8_lhTLTgUeu9WN3FL4Q7KmwAph2UEmpPT_vqw'
+];
 function Home() {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 3000); // Change every 3 seconds
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>Home</div>
+    <section id="home" className="home" style={{ backgroundImage: `url(${images[currentImage]})` }}>
+      <div className="overlay">
+        <h1>Building the Future of Technology</h1>
+      </div>
+    </section>
   )
 }
 
