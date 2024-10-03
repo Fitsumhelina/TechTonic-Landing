e7c425bc705f5cbec726803d4fdf54eafb8e4cbb
Fitsumhelina <permin1501424@gmail.com>
2024-10-03 20:37:05 +0300

    app component built

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import './scss/App.scss'
+import Header from './components/Header'
+import Home from './components/Home'
+import About from './components/About'
+import Contactus from './components/Contactus'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
- 
-     
+     <Header/>
+     <Home/>
+     <About/>
+     <Contactus/>
+     <Footer/>
     </>
   )
 }
