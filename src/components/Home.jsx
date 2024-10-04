1cecbfc8978f819dd04a7c4ff6bd3bc4765732ae
Fitsumhelina <permin1501424@gmail.com>
2024-10-04 13:10:33 +0300

    flip word added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,20 @@
-// import React from 'react';
-// import '../scss/Home.scss';
-// import Header from './Header';
-
-
-"use client";
-import React from "react";
-import { BackgroundGradientAnimation } from "./ui/background-gradient-animation";
-
-import Header from "./Header";
+import React from 'react';
+import '../scss/Home.scss';
+import { FlipWords } from "./ui/flip-words";
+ 
 function Home() {
-  const images = [
-    "https://media.licdn.com/dms/image/v2/D4E22AQGQR5U9MekakQ/feedshare-shrink_1280/feedshare-shrink_1280/0/1703749819916?e=1730937600&v=beta&t=XX9Eg7M0aSYHxOGuRI24whY-UVYEGjDmhktBH5JHUUw",
-    "https://media.licdn.com/dms/image/v2/D4E22AQFcaaWl2qchsA/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1709655117385?e=1730937600&v=beta&t=AR_JjhKJaZ8P-fzYyR3zmF2yQKse4vcWzo-3q0w1GlI",
-    "https://media.licdn.com/dms/image/v2/D4E22AQEWXz-DPdQpTA/feedshare-shrink_1280/feedshare-shrink_1280/0/1709655113033?e=1730937600&v=beta&t=miJAE2GIukKfARS5ja4Ll-JANP20iGw77f2tI4ayvnE",
-  ];
+  const words = ["better", "cute", "beautiful", "modern"];
 
   return (
-    <BackgroundGradientAnimation>
-      <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-          Gradients X Animations
-        </p>
+    <>
+    <div className="home">
+      <div className="text">
+      <h1>Welcome To TechTonic </h1>
+      
+      <h2>WE are<FlipWords words={words} /> </h2>
       </div>
-    </BackgroundGradientAnimation>
+    </div>
+    </>
   )
 }
 
