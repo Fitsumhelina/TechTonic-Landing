42763def7f20c464a886bdc5543cd28432b18726
Fitsumhelina <permin1501424@gmail.com>
2024-10-04 12:29:08 +0300

    homepage changed to gradiant

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,10 @@
 
 
 "use client";
-import { motion } from "framer-motion";
 import React from "react";
-import { ImagesSlider } from "./ui/images-slider";
+import { BackgroundGradientAnimation } from "./ui/background-gradient-animation";
+
+import Header from "./Header";
 function Home() {
   const images = [
     "https://media.licdn.com/dms/image/v2/D4E22AQGQR5U9MekakQ/feedshare-shrink_1280/feedshare-shrink_1280/0/1703749819916?e=1730937600&v=beta&t=XX9Eg7M0aSYHxOGuRI24whY-UVYEGjDmhktBH5JHUUw",
@@ -15,28 +16,13 @@ function Home() {
   ];
 
   return (
-    <>
-      <ImagesSlider className="h-[40rem]" images={images}>
-        <motion.div
-          initial={{
-            opacity: 0,
-            y: -80,
-          }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
-          transition={{
-            duration: 0.6,
-          }}
-          className="z-50 flex flex-col justify-center items-center"
-        >
-          <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
-            The hero section slideshow <br /> nobody asked for
-          </motion.p>
-        </motion.div>
-      </ImagesSlider>
-    </>
+    <BackgroundGradientAnimation>
+      <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+          Gradients X Animations
+        </p>
+      </div>
+    </BackgroundGradientAnimation>
   )
 }
 
