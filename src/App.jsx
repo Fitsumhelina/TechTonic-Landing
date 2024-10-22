dd6789c711d54539dd22c5bc1203ad0aa9e9afbd
Fitsumhelina <permin1501424@gmail.com>
2024-10-22 12:32:02 +0300

    smooth transition and scroll added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
-import { useState } from 'react'
-import './scss/App.scss'
-import Header from './components/Header'
-import Home from './components/Home'
-import About from './components/About'
-import Footer from './components/Footer'
-import Counter from './components/Counter'
-import Service from './components/Service'
-import Teams from './components/Teams'
-import Gallery from './components/Gallery'
-import Partners from './components/Partners'
+import React from 'react';
+import './scss/App.scss';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Footer from './components/Footer';
+import Counter from './components/Counter';
+import Service from './components/Service';
+import Teams from './components/Teams';
+import Gallery from './components/Gallery';
+import Partners from './components/Partners';
 
 function App() {
-
   return (
     <>
-     <Header/>
-     <Home/>
-     <Counter/>
-     <Service/>
-     <About/>
-     <Gallery/>
-     <Teams/>
-     <Partners/>
-     <Footer/>
+      <Header />
+      <div id="home">
+        <Home />
+      </div>
+      <div id="counter">
+        <Counter />
+      </div>
+      <div id="about">
+        <Service />
+        <About />
+        <Gallery />
+        <Teams />
+        <Partners />
+      </div>
+      <div id="footer">
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import '../scss/Header.scss'
+import React from 'react';
+import { Link } from 'react-scroll';
+import '../scss/Header.scss';
 
 function Header() {
   return (
     <>
-    <header className="header">
+      <header className="header">
         <div className="one">
-        <img src="https://media.licdn.com/dms/image/v2/D4E0BAQFtoH7AFayJWw/company-logo_200_200/company-logo_200_200/0/1703627777248/techtonic_tribe_logo?e=1735776000&v=beta&t=k4OrPdu23X5RfbMtxD92Wt_-75jKkqtwccqFhyn4Tbo" alt="" />
+          <Link to="home" spy={true} smooth={true} duration={500}>
+            <img
+              src="https://media.licdn.com/dms/image/v2/D4E0BAQFtoH7AFayJWw/company-logo_200_200/company-logo_200_200/0/1703627777248/techtonic_tribe_logo?e=1735776000&v=beta&t=k4OrPdu23X5RfbMtxD92Wt_-75jKkqtwccqFhyn4Tbo"
+              alt="Logo"
+            />
+          </Link>
         </div>
         <div className="two">
-        <a href="#">About Us</a>
-        <a href="#">Contact US</a>
-        <a href="#">Register</a>
+          <Link to="about" spy={true} smooth={true} duration={500}>
+            About Us
+          </Link>
+          <Link to="footer" spy={true} smooth={true} duration={500}>
+            Contact Us
+          </Link>
+          <Link to="counter" spy={true} smooth={true} duration={500}>
+            Register
+          </Link>
         </div>
-    </header>
+      </header>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
