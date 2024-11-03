fa9968dd7834b89bc7c3270fdb82ae15d7b31716
Fitsumhelina <permin1501424@gmail.com>
2024-11-03 13:46:58 +0300

    dev  info complated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,55 +34,58 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <div className="footer-left">
-        <h2>TechTonic Tribe</h2>
-        <p>
-          We believe in a <strong>modern, Educated & Informed</strong> society that lives
-          with updated technology. Support the amazing work of our company.
-        </p>
-        <div className="footer-info">
-          <p><strong>Headquarters:</strong></p>
-          <p>Debre Berhan University </p>
-          <p>Ethiopia, Debre Berhan</p>
-          <p><strong>Phone:</strong> (251) 9660887276</p>
-          <p><strong>Email:</strong> techtonic.inbox@gmail.com</p>
+      <div className="info">
+
+        <div className="footer-left">
+          <h2>TechTonic Tribe</h2>
+          <p>
+            We believe in a <strong>modern, Educated & Informed</strong> society that lives
+            with updated technology. Support the amazing work of our company.
+          </p>
+          <div className="footer-info">
+            <p><strong>Headquarters:</strong></p>
+            <p>Debre Berhan University </p>
+            <p>Ethiopia, Debre Berhan</p>
+            <p><strong>Phone:</strong> (251) 9660887276</p>
+            <p><strong>Email:</strong> techtonic.inbox@gmail.com</p>
+          </div>
+          <div className="social-icons">
+            <a href="https://t.me/TechTonicTribe"><FaTelegram /></a>
+            <a href="https://www.instagram.com/tech_tonic_tribe?igsh=MTY5NDZyYXkxdTZhOA=="><FaInstagram /></a>
+            <a href="https://www.linkedin.com/company/techtonic-tribe/"><FaLinkedin /></a>
+            <a href="https://www.tiktok.com/@tech_tonic_tribe"><FaTiktok /></a>
+          </div>
         </div>
-        <div className="social-icons">
-          <a href="https://t.me/TechTonicTribe"><FaTelegram /></a>
-          <a href="https://www.instagram.com/tech_tonic_tribe?igsh=MTY5NDZyYXkxdTZhOA=="><FaInstagram /></a>
-          <a href="https://www.linkedin.com/company/techtonic-tribe/"><FaLinkedin /></a>
-          <a href="https://www.tiktok.com/@tech_tonic_tribe"><FaTiktok /></a>
+        <div className="footer-right">
+          <h2>CONTACT US</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="fullName"
+              placeholder="Full Name"
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              onChange={handleChange}
+              required
+            />
+            <textarea
+              name="message"
+              placeholder="Message..."
+              onChange={handleChange}
+              required
+            />
+            <button type="submit">SEND</button>
+          </form>
         </div>
-      </div>
-      <div className="footer-right">
-        <h2>CONTACT US</h2>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="fullName"
-            placeholder="Full Name"
-            onChange={handleChange}
-            required
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Email Address"
-            onChange={handleChange}
-            required
-          />
-          <textarea
-            name="message"
-            placeholder="Message..."
-            onChange={handleChange}
-            required
-          />
-          <button type="submit">SEND</button>
-        </form>
       </div>
       <div className="footer-center">
         <img src={logo} alt="logo" />
-        <p>fitsum helina's portfolio &copy; 2024</p>
+        <p>developed by fitsum helina  &copy; sep, 2024</p>
       </div>
     </footer>
   );
